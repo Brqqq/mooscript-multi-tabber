8780f9bc409e140bd549d6aa03329de35de56840
Burak <b.karatas0@gmail.com>
2021-03-14 13:11:08 +0100

    Sorting no lead factory and no crew on the bottom

--- src/options/sorting.js
+++ src/options/sorting.js
@@ -1,13 +1,20 @@
-const sortAlphabetically = (accountList, propToCompare, isAsc) => {
+const sortAlphabetically = (accountList, propToCompare, isAsc, zeroValue) => {
+    console.log(zeroValue)
     const sorted = accountList
-        .sort((a, b) => a[propToCompare]?.toString().localeCompare(b[propToCompare].toString()));
+        .sort((a, b) => {
+            if(a[propToCompare] === zeroValue && b[propToCompare] === zeroValue) return 0;
+            else if(a[propToCompare] === zeroValue) return 1;
+            else if(b[propToCompare] === zeroValue) return -1;
+
+            return a[propToCompare]?.toString().localeCompare(b[propToCompare].toString())
+        });
     
     return isAsc ? sorted : sorted.reverse();
 }
 
 const sortNumbers = (accountList, propToCompare, isAsc) => {
     const sorted = accountList
-        .sort((a, b) => +a[propToCompare] - +b[propToCompare]);
+        .sort((a, b) => (+a[propToCompare] || 0) - (+b[propToCompare] || 0));
     
     return isAsc ? sorted : sorted.reverse();
 }
@@ -37,7 +44,8 @@ export const sortAccounts = (accountList, propToSort, isAsc) => {
     if(numericalProps.includes(propToSort)) {
         return sortNumbers(accountList, propToSort, isAsc);
     } else if(alphabeticalProps.includes(propToSort)) {
-        return sortAlphabetically(accountList, propToSort, isAsc);
+        let zeroValue = propToSort === "crew" ? "None" : undefined;
+        return sortAlphabetically(accountList, propToSort, isAsc, zeroValue);
     } else if(propToSort === "rank") {
         return rankSort(accountList, propToSort, isAsc);
     }
