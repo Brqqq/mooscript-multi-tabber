8fe366c8c632435bfc4f47437ea59eef199e0278
Burak <b.karatas0@gmail.com>
2021-03-14 18:22:48 +0100

    Added a remember password function and focusing on the email input after submit

--- src/options/NewUser.js
+++ src/options/NewUser.js
@@ -3,18 +3,25 @@ import React from "react";
 const NewUser = ({ onSubmit }) => {
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
-
+    const [rememberPassword, setRememberPassword] = React.useState(false);
+    const emailRef = React.useRef();
     return <>
         <h3>Add new account</h3>
-        <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.currentTarget.value)} />
+        <input ref={emailRef} type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.currentTarget.value)} />
         <input style={{ marginLeft: 6 }} type="text" placeholder="Password" value={password} onChange={e => setPassword(e.currentTarget.value)} />
+        <input id="rememberpassword" type="checkbox" onChange={e => setRememberPassword(e.currentTarget.checked)} />
+        <label htmlFor="rememberpassword">Remember password</label>
+
         <button
             style={{ marginLeft: 6 }}
             onClick={(e) => {
                 e.preventDefault();
                 onSubmit(email, password);
                 setEmail("");
-                setPassword("");
+                if (!rememberPassword) {
+                    setPassword("");
+                }
+                emailRef.current?.focus();
             }}>Add</button>
     </>
 }
